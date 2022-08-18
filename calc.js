26065619309b0c9f7d05a14e6915ce0be87c2d32
Intejar <meherabintejar@gmail.com>
2022-08-18 19:24:14 -0400

    number in display added

--- calc.js
+++ calc.js
@@ -1,18 +1,18 @@
-function getIntegerValue(inputValue){
+function getIntegerValue(inputValue) {
     const getStringValue = document.getElementById(inputValue);
     const makeInteger = parseFloat(getStringValue.innerText);
     return makeInteger;
 }
-document.getElementById('ac-button').addEventListener('click', function(){
+document.getElementById('ac-button').addEventListener('click', function () {
     const displayNumber = document.getElementById('display-text');
-    displayNumber.innerText = '0';
-    
+    displayNumber.innerText = ' ';
+
 })
-function showNumber(number){
+function showNumber(number) {
+    const displayPrev = document.getElementById('display-text');
     const inputNumber = document.getElementById(number);
-    inputNumber.addEventListener('click', function(){
-        const displayPrev = document.getElementById('display-text');
-        displayPrev.innerText = inputNumber.innerText;
+    inputNumber.addEventListener('click', function () {
+        displayPrev.innerText += inputNumber.innerText;
     })
 }
 showNumber('number-0');
@@ -25,3 +25,4 @@ showNumber('number-6');
 showNumber('number-7');
 showNumber('number-8');
 showNumber('number-9');
+showNumber('plus-btn');
